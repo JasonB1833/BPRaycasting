f1afccf5d636a5bcbd205e04305070b9212bc444
JasonB1833 <112956654+JasonB1833@users.noreply.github.com>
2024-06-26 02:04:55 -0400

    Add files via upload
    - Added code that should create a 3D perspective, removed player drawing
    
    TODO:
    - add comments
    - test run / debug current build (its 2AM)

--- BPRaycasting/BPRaycasting/BPRaycasting.cpp
+++ BPRaycasting/BPRaycasting/BPRaycasting.cpp
@@ -35,7 +35,7 @@ void draw_rectangle(std::vector<uint32_t>& img, const size_t img_w, const size_t
 		for (size_t j = 0; j < h; j++) {
 			size_t cx = x + i;
 			size_t cy = y + j;
-			assert(cx < img_w && cy < img_h);
+      if (cx>= img_w || cy>= img_h) continue; // not checking negative values, (unsigned variables) 
 			img[cx + cy * img_w] = color;
 		}
 	}
@@ -43,9 +43,9 @@ void draw_rectangle(std::vector<uint32_t>& img, const size_t img_w, const size_t
 
 int main()
 {
-	const size_t win_w = 512; // image width
+	const size_t win_w = 1024; // image width
 	const size_t win_h = 512; // image width
-	std::vector<uint32_t> framebuffer(win_w * win_h, 255); // the image itself, init to red
+	std::vector<uint32_t> framebuffer(win_w * win_h, pack_color(255,255,255)); // the image itself, init to white 
 
 	const size_t map_w = 16; // map width
 	const size_t map_h = 16; // map height 
@@ -71,18 +71,9 @@ int main()
 	float player_x = 3.456; // player x position
 	float player_y = 2.345; // player y position
 	float player_a = 1.523; // player view direction
-  const float fov = M_PY/3; // field of view 
+  const float fov = M_PI/3; // field of view 
 
-	for (size_t j = 0; j < win_h; j++) { // fill the window with color gradients
-		for (size_t i = 0; i < win_w; i++) {
-			uint8_t r = 255 * j / float(win_h); // varies between 0 and 255 as j sweeps the vertical
-			uint8_t g = 255 * i / float(win_w); // varies between 0 and 255 as i sweeps the horizontal
-			uint8_t b = 0;
-			framebuffer[i + j * win_w] = pack_color(r, g, b);
-		}
-	}
-	// code for the map layout (in this case blue rectangles that are 'obstacles')
-	const size_t rect_w = win_w / map_w;
+	const size_t rect_w = win_w / (map_w*2);
 	const size_t rect_h = win_h / map_h;
 	for (size_t j = 0; j < map_w; j++) {
 		for (size_t i = 0; i < map_w; i++) {
@@ -93,19 +84,27 @@ int main()
 		}
 	}
   
-
-    for (size_t i=0; i<win_w; i++) {// draw the visibility cone 
-      float angle = player_a-fov/2 + fov*i/float(win_w);
-    }
-
+  for (size_t i=0; i<win_w/2; i++) { // draw the visibility cone and 3d view
+  
+    float angle = player_a - fov/2 + fov*i/float(win_w/2);
+  
     for (float t=0; t<20; t+=.05) {
       float cx = player_x + t*cos(angle);
       float cy = player_y + t*sin(angle);
-      if (map[int(cx) + int (cy) *map_w]!=' ')break;
+    
 
       size_t pix_x = cx*rect_w;
       size_t pix_y = cy*rect_h;
-      frambuffer[pix_x + pix_y *win_w] = pack_color(255,255,255);
+      framebuffer[pix_x + pix_y *win_w] = pack_color(160,160,160);
+      // this draws visibility continue
+
+      if (map[int(cx)+int(cy)*map_w]!=' ') { // our ray touches a wall, so draw the vertical column to create an illusion of 3D 
+      
+        size_t column_height = win_h/t;
+        draw_rectangle(framebuffer, win_w, win_h, win_w/2+i, win_h/2-column_height/2, 1, column_height, pack_color(0,255,255));
+        break;
+      }
+
     }
 	}
 	
